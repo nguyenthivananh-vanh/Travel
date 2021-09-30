91364caf59384fd391ac2eab931f68d7812eb30e
trang.danghuyen.ncc <trang.danghuyen.ncc@gmail.com>
2021-09-30 23:16:42 +0700

    gatay

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -1,16 +1,12 @@
 
-<nav class="blue darken-2">
+<nav class="blue darken-2 header-mobile">
     <div class="container-fuild">
       <div class="nav-wrapper">
         <a href="admin/adminHome" class="brand-logo" id="adminH">Admin</a>
         <a  data-activates="side-nav" class="button-collapse show-on-large right"id="btnShowSideNav" onclick="showSidebar()">
           <i class="material-icons">menu</i>
         </a>
-        <ul class="right hide-on-med-and-down">
-          <!-- <li class="active">
-            <a href="index.html">Dashboard</a>
-          </li> -->
-          
+        <!-- <ul class="right hide-on-med-and-down">
           <li>
             <a href="home/home/1">Trang chủ</a>
           </li>
@@ -36,49 +32,57 @@
             <a href="admin/thongke/list">Thống kê</a>
           </li>
          
-          <!-- <li>
-            <a href="#" data-activates="mobile-nav" class="button-collapse" (click)="showNav()">
-              <i class="material-icons">menu</i>
-            </a>
-          </li> -->
+         
 
-        </ul>
+        </ul> -->
         <!-- Side-nav -->
 
-          <ul id="side-nav-admin" class="side-nav-admin" >
-            <li>
-              <div class="user-view" >
-              <a href="javascript:void(0)" class="admin-btnClose-sidenav" onclick="showSidebar()"><i class="fas fa-times"></i></a>
-                <div class="background">
+        <ul id="side-nav-admin-mobile" class="side-nav-admin-mobile" >
+          <li>
+              <div class="user-view">
+                    <a href="">Admin</a>
+                    <!-- <a  class="admin-btnClose-sidenav" onclick="showSidebar()"><i class="fas fa-times"></i></a> -->
+                {{-- <div class="background">
                   <img src="upload/users/background-admin.jpg" alt="ocean-background">
                 </div>
-                <a href="#" class="admin-img-sidenav">
-                  <img src="upload/users/ava-admin.jpg" alt="person1.jpg" class="circle" style='width:50px; height:50px'>
-                </a>
-                <a href="#" class="admin-name-sidenav white-text">
-                  <span>Admin My VietNam</span>
-                </a>
-                <a href="#" class="admin-email-sidenav white-text">
-                  <span>myvietnam@gmail.com</span>
-                </a>
-              </div>
-            </li>
+                <div class="ml-2"  style="margin-top:20%; margin-left:20px">
+                  <a href="#" class="admin-img-sidenav mt-4">
+                    <img src="upload/users/ava-admin.jpg" alt="person1.jpg" class="circle avatar-user" style='width:50px; height:50px'>
+                  </a>
+                  <a href="#" class="admin-name-sidenav white-text">
+                    <p class="">Admin My VietNam</p>
+                  </a>
+                  <a href="#" class="admin-email-sidenav white-text">
+                    <p>myvietnam@gmail.com</p>
+                  </a>
+                </div>
+              </div> --}}
+          </li>
             
-            <li>
-              <a href="posts.html">Posts</a>
-            </li>
-            <li>
-              <a href="comments.html">Comments</a>
-            </li>
-            <li>
-              <a href="users.html">Users</a>
-            </li>
-            <li>
-              <a class="subheader">Quản lí tài khoản</a>
-            </li>
-            <li>
-              <a href="login.html" class="waves-effect">Đăng xuất</a>
-            </li>
+          <li>
+            <a href="home/home/1">Trang chủ</a>
+          </li>
+          <li>
+            <a href="admin/vungmien/list">Vùng miền</a>
+          </li>
+          <li>
+            <a href="admin/dacdiem/list">Đặc điểm</a>
+          </li>
+          <li>
+            <a href="admin/diadiem/list">Địa điểm</a>
+          </li>
+          <li>
+            <a href="admin/user/list">Users</a>
+          </li>
+          <li>
+            <a href="admin/comment/list">Comments</a>
+          </li>
+          <li>
+            <a href="admin/thongke/list">Thống kê</a>
+          </li>
+          <li>
+            <a >Đăng xuất</a>
+          </li>
           </ul>       
       </div>
     </div>
